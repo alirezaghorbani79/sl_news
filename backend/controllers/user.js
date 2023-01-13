e2bc01896387bf8f05650bb60a056cdde174c4be
Alireza Ghorbani <bounty.1379@gmail.com>
2023-01-13 15:39:16 +0330

    alireza

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,18 +1,20 @@
 const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
+const jwt = require('jsonwebtoken')
+
 
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
+        expiresIn: 3600,
     })
 }
 
 const createSendToken = (user, statusCode, req, res) => {
-    const token = signToken(user._id)
+    const token = signToken(user.id)
   
     res.cookie('jwt', token, {
       expires: new Date(
-        Date.now + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        Date.now() + 3600 * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
       secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
@@ -32,11 +34,27 @@ const createSendToken = (user, statusCode, req, res) => {
 
 exports.signUp = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
-        name: req.boyd.name,
+        name: req.body.name,
         email: req.body.email,
         password: req.body.password,
     })
 
     console.log(newUser)
-    // createSendToken(newUser, 201, req, res)
+    createSendToken(newUser, 201, req, res)
+})
+
+
+exports.login = catchAsync(async (req, res, next) => {
+    const user = await User.findOne({ where: { email: req.body.email }})
+
+    if(!user) {
+        // return error
+    }
+
+    if (user.password !== req.body.password) {
+        // return error
+    }
+
+    createSendToken(user, 200, req, res)
+
 })
