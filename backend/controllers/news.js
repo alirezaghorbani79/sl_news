848150767c3a4fbc89f800e181f6be490b49f6e3
Alireza Ghorbani <bounty.1379@gmail.com>
2023-01-13 18:09:41 +0330

    alireza

---
+++ backend/controllers/news.js
@@ -0,0 +1,73 @@
+const catchAsync = require("../utils/catchAsync");
+const { News, validateUpdate, validateCreate } = require('../models/news');
+
+
+exports.getAllNews = catchAsync(async (req, res) => {
+    // req.params.orderBy
+    const news = await News.findAll()
+    res.send(news);
+})
+
+exports.getOneNews = catchAsync(async (req, res) => {
+    const news = await News.findOne({
+        where: {
+            id: req.params.id,
+        }
+    })
+    res.send(news);
+})
+
+exports.createNews = catchAsync(async (req, res) => {
+    const { error } = validateCreate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const news = await News.create({
+        title: req.body.title,
+        body: req.body.body,
+        picture: req.body.picture
+    })
+    res.send(news);
+})
+
+exports.updateNews = catchAsync(async (req, res) => {
+    const { error } = validateUpdate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const news = await News.findOne({
+        where: {
+            id: req.params.id,
+        }
+    })
+
+    if (!news) return res.status(404).send('The news with the given ID was not found.');
+
+    await News.update(
+        {
+            title: req.body.title,
+            body: req.body.body
+        }, {
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.send('News was updated successfully.');
+})
+
+exports.deleteNews = catchAsync(async (req, res) => {
+    const news = await News.findOne({
+        where: {
+            id: req.params.id,
+        }
+    })
+
+    if (!news) return res.status(404).send('The news with the given ID was not found.');
+
+    await News.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    res.send('News was deleted successfully.');
+})
--- backend/routes/news.js
+++ backend/routes/news.js
@@ -2,77 +2,14 @@ const { News, validateUpdate, validateCreate } = require('../models/news');
 const express = require('express');
 const auth = require('../middleware/auth')
 const admin = require('../middleware/admin')
+const newsController = require('../controllers/news')
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    // req.params.orderBy
-    const news = await News.findAll()
-    res.send(news);
-});
-
-router.get('/:id', async (req, res) => {
-    const news = await News.findOne({
-        where: {
-            id: req.params.id,
-        }
-    })
-    res.send(news);
-});
-
-router.post('/', [auth, admin], async (req, res) => {
-    const { error } = validateCreate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const news = await News.create({
-        title: req.body.title,
-        body: req.body.body,
-        picture: req.body.picture
-    })
-    res.send(news);
-});
-
-router.put('/:id', [auth, admin], async (req, res) => {
-    const { error } = validateUpdate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const news = await News.findOne({
-        where: {
-            id: req.params.id,
-        }
-    })
-
-    if (!news) return res.status(404).send('The news with the given ID was not found.');
-
-    await News.update(
-        {
-            title: req.body.title,
-            body: req.body.body
-        }, {
-        where: {
-            id: req.params.id
-        }
-    });
-
-    res.send('News was updated successfully.');
-});
-
-router.delete('/:id', [auth, admin], async (req, res) => {
-    const news = await News.findOne({
-        where: {
-            id: req.params.id,
-        }
-    })
-
-    if (!news) return res.status(404).send('The news with the given ID was not found.');
-
-    await News.destroy({
-        where: {
-            id: req.params.id
-        }
-    });
-
-    res.send('News was deleted successfully.');
-});
+router.get('/', newsController.getAllNews);
+router.get('/:id', newsController.getOneNews);
+router.post('/', [auth, admin], newsController.createNews);
+router.put('/:id', [auth, admin], newsController.updateNews);
+router.delete('/:id', [auth, admin], newsController.deleteNews);
 
 
 
